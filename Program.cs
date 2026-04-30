8926db21085f587252d596d50c1df1c8679783a8
kunlun <kunlun7@yandex.ru>
2026-04-30 19:55:41 +0700

    Создали конструктор с аргументами в классе APoint и зменили прямую иниицализацию полей на иниц-ю в конструкторе.

--- Program.cs
+++ Program.cs
@@ -11,17 +11,14 @@ namespace TetrisConsole
 			Console.SetWindowSize(winWidth, winHeight);
 			Console.SetBufferSize(winWidth, winHeight);
 
-			APoint p1 = new APoint();
-			p1.x = 3;
-			p1.y = 4;
-			p1.c = '*';
+			APoint p1 = new APoint(3, 4, '*');
 			p1.Draw();
 
-			APoint p2 = new APoint();
-			p2.x = 3;
-			p2.y = 4;
-			p2.c = '*';
-			p2.Draw();
+			//APoint p2 = new APoint();
+			//p2.x = 3;
+			//p2.y = 4;
+			//p2.c = '*';
+			//p2.Draw();
 
 
 
