21429970328393784cbbce91ce054d55ee60282e
kunlun <kunlun7@yandex.ru>
2026-04-30 20:02:09 +0700

    Для второй точки применили инициализацию полей в конструкторе без параметров (быстрая иниц-ция при создании объекта).

--- Program.cs
+++ Program.cs
@@ -14,10 +14,7 @@ namespace TetrisConsole
 			APoint p1 = new APoint(3, 4, '*');
 			p1.Draw();
 
-			APoint p2 = new APoint();
-			p2.x = 3;
-			p2.y = 4;
-			p2.c = '*';
+			APoint p2 = new APoint () { x = 5, y = 6, c = '#'};
 			p2.Draw();
 
 
