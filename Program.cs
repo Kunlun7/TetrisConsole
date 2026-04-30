c50f4155d4667787c7859ac8085ff8eeca03a064
kunlun <kunlun7@yandex.ru>
2026-04-30 19:43:18 +0700

    Задали размер консоли и ее буфера. Создали функцию Draw и вывели на консоль 2 тчки.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace TetrisConsole
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int winWidth = 40;
+			int winHeight = 30;
+			Console.SetWindowSize(winWidth, winHeight);
+			Console.SetBufferSize(winWidth, winHeight);
+
+			int x1 = 3;
+			int y1 = 4;
+			char c1 = '*';
+			Draw(x1, y1, c1);
+
+
+			int x2 = 5;
+			int y2 = 6;
+			char c2 = '*';
+			Draw(x2, y2, c2);
+
+
+
+			Console.ReadLine();
 		}
+
+		static public void Draw(int x, int y, char c)
+		{
+
+			Console.SetCursorPosition(x, y);
+			Console.Write(c);
+		}
+
 	}
 }
