8a438190a996ce901851f2efbbec0b85643fe31f
kunlun <kunlun7@yandex.ru>
2026-05-03 00:06:18 +0700

    создали интерфейс IDrawer и реализацию AConsoleDrawer.

---
+++ AConsoleDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsole
+{
+	class AConsoleDrawer : IDrawer
+	{
+		public const char FigureChar = 'O';
+		public const char FieldChar = '.';
+
+		public void ClearPoint(int x, int y)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(FigureChar);
+			Console.SetCursorPosition(0, 0);
+		}
+
+		public void DrawPoint(int x, int y)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(FieldChar);
+			Console.SetCursorPosition(0, 0);
+		}
+	}
+}
---
+++ IDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsole
+{
+	interface IDrawer
+	{
+		void DrawPoint(int x, int y);
+		void ClearPoint(int x, int y);
+	}
+}
