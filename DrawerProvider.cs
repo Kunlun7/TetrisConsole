3bf2cd434ae82fd6e92c301ca652532b470f7c2d
kunlun <kunlun7@yandex.ru>
2026-05-03 00:17:52 +0700

    создали класс DrawerProvider для хранения текущего Drawer

---
+++ DrawerProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsole
+{
+	static class DrawerProvider
+	{
+		private static IDrawer _drawer = new AConsoleDrawer();
+
+		public static IDrawer Drawer
+		{
+			get
+			{
+				return _drawer;
+			}
+		}
+	}
+}
